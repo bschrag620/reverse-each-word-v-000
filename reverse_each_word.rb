e2614ec4a1b777e9031015981462e74c9c53de38
Brad Schrag <brad.schrag@gmail.com>
2018-11-05 00:53:49 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   array = sentence.split(" ")
   reverse_sentence = ""
   array.each do | word |
-    reverse_sentence += word.reverse
+    reverse_sentence += word.reverse + ' '
   end
   reverse_sentence
 end
