b72edcef603b235fb89cb6402611ae2b17492468
Brad Schrag <brad.schrag@gmail.com>
2018-11-05 01:00:50 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,7 @@ def reverse_each_word(sentence)
   array = sentence.split(" ")
   reverse_array = []
   array.collect do | word |
-    word.reverse.join(" ")
+    reverse_array << word.reverse
   end
+  reverse_array.join(' ')
 end
